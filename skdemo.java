4522356139c24a888df6b47155f30c6badc9331b
jrbryant55 <jrbryant@ucsd.edu>
2022-05-01 23:34:20 -0700

    commit1

--- skdemo.java
+++ skdemo.java
@@ -1,5 +1,6 @@
 public class skdemo{
 
-
-
+public static int multiply(int x, int y){
+    return x*y;
+    }
 }
---
+++ skdemotests.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skdemotest{
+
+    @Test
+    public void multiply(){
+        assertEquals(7, skdemo.multiply(2,3));
+    }
+}
